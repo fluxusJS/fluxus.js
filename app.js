34b8a0bed36e7e45cdcdd416ff8c837492cf3e75
Jesse Cravens <jesse.cravens@frogdesign.com>
2012-05-22 19:31:53 -0500

    added spectator and performer views, with websockets, google maps, pusher, and useragent

--- app.js
+++ app.js
@@ -4,34 +4,30 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , io = require('socket.io')
+  , useragent = require('useragent')
+  , gm = require('googlemaps')
+  , pusher = require('node-pusher');
 
 var app = module.exports = express.createServer();
 
-// HTML Configuration
-	// app.configure(function(){
-	//   app.use(express.static(__dirname + '/public'));
-	// 
-	//   // disable layout
-	//   app.set("view options", {layout: false});
-	// 
-	//   // make a custom html template
-	//   app.register('.html', {
-	//     compile: function(str, options){
-	//       return function(locals){
-	//         return str;
-	//       };
-	//     }
-	//   });
-	// });
-	// 
-	// app.configure('development', function(){
-	//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-	// });
-	// 
-	// app.configure('production', function(){
-	//   app.use(express.errorHandler());
-	// });
+// Clients is a list of users who have connected
+var clients = [];
+
+///////////////////////////////////////////////////////////////////// SEND() UTILITY
+function send(message) { 
+
+  clients.forEach(function(client) {
+      client.send(message);
+  });
+}
+
+///////////////////////////////////////////////////////////////////// DATE UTILITY
+function getTime(){
+	var currentTime = new Date();
+	return currentTime;
+}
 
 // JADE Configuration ////////////////////////////////////////////////////////////////
 	app.configure(function(){
@@ -55,49 +51,63 @@ var app = module.exports = express.createServer();
 // Jade Routes ////////////////////////////////////////////////////////////////////////
 	
 	app.get('/', routes.index);
-	
-	// app.get('/', function(req, res){
-	//   res.render("index.jade");
-	// });
-	
-    app.get('/example1', routes.example1);
-	
-	// app.get('/example1', function(req, res){
-	//   res.render("example1.jade");
-	// });
-	
-	app.get('/example2', routes.example2);
-	app.get('/example3', routes.example3);
-
-
-// HTML Routes
-	// app.get('/', function(req, res){
-	//   res.render("index.html");
-	// });
-	// app.get('/index.html', function(req, res){
-	//   res.render("index.html");
-	// });
-	// app.get('/scaffolding.html', function(req, res){
-	//   res.render("scaffolding.html");
-	// });
-	// app.get('/base-css.html', function(req, res){
-	//   res.render("base-css.html");
-	// });
-	// app.get('/components.html', function(req, res){
-	//   res.render("components.html");
-	// });
-	// app.get('/javascript.html', function(req, res){
-	//   res.render("javascript.html");
-	// });
-	// app.get('/less.html', function(req, res){
-	//   res.render("less.html");
-	// });
-	// app.get('/download.html', function(req, res){
-	//   res.render("download.html");
-	// });
-	// app.get('/examples.html', function(req, res){
-	//   res.render("examples.html");
-	// });
+	app.get('/spectator', routes.spectator);
+	app.get('/performer', routes.performer);
 
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+
+///////////////////////////////////////////////////////////////////// SOCKET.IO SERVER
+var sio = io.listen(app);
+// console.log(sio.settings);
+
+///////////////////////////////////////////////////////////////////// SOCKET.IO WEB SOCKETS
+sio.sockets.on('connection', function(client) {
+  // For each connection made add the client to the array of clients.
+  console.log('server connection EVENT FIRED');
+  //console.log('CLIENT connected'); 
+  console.log('CLIENT ID: ' + client.id + ', TRANSPORT MECHANISM: ' + sio.transports[client.id].name);
+
+///////////////////////////////////////////////////////////////////// BUILD CLIENTS LIST
+  clients.push(client);
+  console.log(clients.length);
+
+  send(JSON.stringify({ "clients": clients.length }));
+
+  // log each clients id
+  clients.forEach(function(client) {
+
+    console.log('CLIENT ID: ' + client.id);
+    // console.log(client);
+
+  });
+
+  client.on('disconnect', function () {
+    console.log('disconnect EVENT FIRED');
+	console.log(clients.length)
+	var index = clients.indexOf(client.id);
+	console.log(index)
+	clients.splice(index, 1);
+	console.log(clients.length)
+  });
+
+  client.on('geo', function(data) {
+	console.log('geo MESSAGE received');
+	console.log(data);
+	var loc = data.lat + "," + data.long
+	console.log(loc)
+	gm.reverseGeocode(loc, function(err, data){
+
+	  var city = data.results[0].address_components[2].long_name;	
+	  var state = data.results[0].address_components[4].long_name;
+	  var loc = city + ", " + state; 
+	  send(JSON.stringify({ "loc": loc }));
+	});
+  });
+});  
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,10 @@ exports.index = function(req, res){
   res.render('index', { title: 'Index' })
 };
 
-exports.example1 = function(req, res){
-  res.render('example1', { title: 'Example1' })
+exports.performer = function(req, res){
+  res.render('performer', { title: 'Performer' })
 };
 
-exports.example2 = function(req, res){
-  res.render('example2', { title: 'Example2' })
-};
-
-exports.example3 = function(req, res){
-  res.render('example3', { title: 'Example3' })
+exports.spectator = function(req, res){
+  res.render('spectator', { title: 'Spectator' })
 };
